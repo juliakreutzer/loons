3ba2c624f8bcfe19f9d6f8ae419cec1965a9d95b
julia <juliakreutzer1@aol.com>
2016-02-03 19:44:13 +0100

    added original source to chernoff face code

--- mpl_cfaces.py
+++ mpl_cfaces.py
@@ -1,5 +1,7 @@
 from pylab import *
 
+## Code by Abraham Flaxman:  https://gist.github.com/aflaxman/4043086 ##
+
 def cface(ax, x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18):
     # x1 = height  of upper face
     # x2 = overlap of lower face
